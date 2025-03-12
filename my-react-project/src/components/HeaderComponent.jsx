a18901b7069d12669c390ff892469c273b6bd7c9
dawcarlosp <dawcarlospereira@gmail.com>
2025-03-12 22:31:06 +0100

    UseState y Tailwind

--- my-react-project/src/components/HeaderComponent.jsx
+++ my-react-project/src/components/HeaderComponent.jsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+
 function HeaderComponent(){
+    let placeHolder = "Escribe aqui";
+    //let number = 1;
+    const [number, setNumber] = useState(1);
+    const [myValue, setmyValue] = useState("");
+    const addOne = () =>{
+        //number++;
+        setNumber(number + 1)
+    }
+    const info = () =>{
+        alert("Gracias por usar la app!");
+    }
+    const handleChange = (ev) =>{
+        if(ev.target.value == "Mesi"){
+            alert("Has dicho Mesi")
+        }
+    }
     return (
-        <header class="flex flex-col items-center justify-center">
-            <h1>CreatiFlow!</h1>
-            <nav class="w-full">
-                <ul class="flex justify-around">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Sobre mi</a></li>
-                    <li><a href="">Sera mejor angular?</a></li>
-                    <li><a href="">O react?</a></li>
+        <header className="flex flex-col items-center justify-center bg-gray-900">
+            <h1 className="text-blue-600 text-9xl font-bold hover:text-purple-600 cursor-pointer" onClick={info}>CreatiFlow!</h1>
+            <h2 className="text-blue-600 text-2xl font-bold hover:text-purple-600 cursor-pointer" onClick={addOne}>Number: {number}</h2>
+            <nav className="w-full text-white">
+                <ul className="flex justify-around">
+                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">Home</a></li>
+                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">Sobre mi</a></li>
+                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">Sera mejor angular?</a></li>
+                    <li className="my-5"><a href="" className="p-2 border  border-blue-600 hover:border-purple-600 rounded-sm">O react?</a></li>
+                    <li className="my-5"><input type="text"className="p-2 border  border-blue-600 hover:border-purple-600 rounded-sm" onChange={handleChange} placeholder={placeHolder} value={myValue}/></li>
                 </ul>
             </nav>
         </header>
