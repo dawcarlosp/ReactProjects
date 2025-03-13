f9d3ba220e7c268ba6e590b4188e49ec4ba5af31
dawcarlosp <dawcarlospereira@gmail.com>
2025-03-13 12:52:45 +0100

    Props de padre a hijos

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import ButtonComponent from './components/ButtonComponent'
 import HeaderComponent from './components/HeaderComponent'
-
+import { useState } from "react";
 function App() {
+  //Variable reactiva
+  const [greetings] = useState("Bienvenidos a mi web");
+  //Variable corriente
+  const links = {
+    home: "Home",
+    blog: "Blog",
+    news: "News",
+    contact: "Contact us"
+  }
   return (
     <>
-      <HeaderComponent></HeaderComponent>
+      <HeaderComponent greetings={greetings} links = {links}></HeaderComponent>
       <ButtonComponent></ButtonComponent>
     </>
   )
--- my-react-project/src/components/HeaderComponent.jsx
+++ my-react-project/src/components/HeaderComponent.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
-function HeaderComponent(){
+function HeaderComponent(props){
+    //Desesctructuración
+    const {greetings, links} = props;
     let placeHolder = "Escribe aqui";
     //let number = 1;
     const [number, setNumber] = useState(1);
-    const [myValue, setmyValue] = useState("");
+    const [myValue] = useState("");
     const addOne = () =>{
         //number++;
         setNumber(number + 1)
@@ -19,14 +21,15 @@ function HeaderComponent(){
     }
     return (
         <header className="flex flex-col items-center justify-center bg-gray-900">
+            <h3>{greetings}</h3>
             <h1 className="text-blue-600 text-9xl font-bold hover:text-purple-600 cursor-pointer" onClick={info}>CreatiFlow!</h1>
             <h2 className="text-blue-600 text-2xl font-bold hover:text-purple-600 cursor-pointer" onClick={addOne}>Number: {number}</h2>
             <nav className="w-full text-white">
                 <ul className="flex justify-around">
-                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">Home</a></li>
-                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">Sobre mi</a></li>
-                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">Sera mejor angular?</a></li>
-                    <li className="my-5"><a href="" className="p-2 border  border-blue-600 hover:border-purple-600 rounded-sm">O react?</a></li>
+                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">{links.home}</a></li>
+                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">{links.blog}</a></li>
+                    <li className="my-5"><a href="" className="p-2 border border-blue-600 hover:border-purple-600 rounded-sm">{links.news}</a></li>
+                    <li className="my-5"><a href="" className="p-2 border  border-blue-600 hover:border-purple-600 rounded-sm">{links.contact}</a></li>
                     <li className="my-5"><input type="text"className="p-2 border  border-blue-600 hover:border-purple-600 rounded-sm" onChange={handleChange} placeholder={placeHolder} value={myValue}/></li>
                 </ul>
             </nav>
